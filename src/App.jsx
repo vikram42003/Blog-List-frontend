89d443a185b0ed6c0b4eb4015c559ba4de379518
vikram(wsl) <vikramjit360@gmail.com>
2024-08-07 21:12:29 +0530

    5.1 - feat: add login form and login functionality
    - add 'LoginForm' Component to ./src/components
    - add ./src/services as login helper file
    - add 'Blog' Component to ./src/components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+
+import blogService from "./services/blogs";
+
+import Blog from "./components/Blog";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null);
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then(blogs => setBlogs(blogs));
+  }, []);
 
-  return (
+  return user === null ? (
+    <LoginForm headerText="log in to application" setUser={setUser} />
+  ) : (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <p>{user.name} logged in</p>
+      {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import loginService from "../services/login";
+
+const LoginForm = ({ headerText, setUser }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async event => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+      console.log("Login Successful!");
+    } catch (exception) {
+      console.log("Login Failed! - \n", exception);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>{headerText}</h2>
+
+      <label htmlFor="username">
+        username &nbsp;
+        <input type="text" id="username" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <br />
+
+      <label htmlFor="password">
+        password &nbsp;
+        <input type="text" id="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <br />
+
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
