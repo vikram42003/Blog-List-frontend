c764c03cd1327189f4298ba77bee7494e400ebe6
vikram(wsl) <vikramjit360@gmail.com>
2024-08-10 22:46:08 +0530

    5.7 - feat: allow user to toggle details about a blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - by {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return showDetails ? (
+    <div style={blogStyle}>
+      {blog.title}{" "}
+      <button type="button" onClick={toggleShowDetails}>
+        hide
+      </button>{" "}
+      <br />
+      {blog.url} <br />
+      {`likes ${blog.likes} `} <button type="button">like</button><br />
+      {blog.author} <br />
+    </div>
+  ) : (
+    <div style={blogStyle}>
+      {blog.title} - by {blog.author}{" "}
+      <button type="button" onClick={toggleShowDetails}>
+        view
+      </button>
+    </div>
+  );
+};
+
+export default Blog;
