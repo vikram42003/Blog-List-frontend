9f25829164af9f48c1409f724a0daa09f618451b
vikram(wsl) <vikramjit360@gmail.com>
2024-08-14 11:25:07 +0530

    5.14 - test: add test for 'Blog' component to check correct content is rendered on clicking 'view' button

--- tests/componentsTests/Blog.test.jsx
+++ tests/componentsTests/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Blog from "../../src/components/Blog";
 
@@ -31,7 +32,6 @@ describe("<Blog />", () => {
         setNotification={mockSetNotification}
       />
     );
-    screen.debug();
     const textToRender = screen.getByText(/Testing blog - by ME/i);
     const toNotRender1 = screen.queryByText(/should not render/);
     const toNotRender2 = screen.queryByText(/likes 10/);
@@ -39,5 +39,32 @@ describe("<Blog />", () => {
     expect(textToRender).toBeDefined();
     expect(toNotRender1).toBeNull();
     expect(toNotRender2).toBeNull();
+  });
+
+  it("blog's URL and number of likes are shown when the button controlling the shown details is clicked", async () => {
+    const testData = {
+      title: "testing in react",
+      author: "some author",
+      url: "some url",
+      likes: 20,
+    };
+
+    render(
+      <Blog
+        user={mockUser}
+        blog={testData}
+        blogs={mockBlogs}
+        setBlogs={mockSetBlogs}
+        setNotification={mockSetNotification}
+      />
+    );
+
+    const button = screen.getByText("view");
+
+    const user = userEvent.setup();
+    await user.click(button);
+
+    screen.getByText(/some url/);
+    screen.getByText(/likes 20/);
   });
 });
